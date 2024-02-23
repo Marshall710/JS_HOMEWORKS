22c22b913825c2e05a8094d41ddd376615d9da91
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-02-23 20:39:25 +0200

    hw_6 completed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 'use strict'
 
-const userName = prompt('What is you name?');
-alert(`Hello, ${userName}! How are you?`);
+//hw_6.1
+let input1 = prompt('input1 text');
+let input2 = prompt('input2 text');
+let input3 = prompt('input3 text');
+
+console.log(input3 + ' ' + input2 + ' ' + input1);
+
+//hw_6_2
+let num = 12345;
+
+console.log(num.toString().split('').join(' '))
