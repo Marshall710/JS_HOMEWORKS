1ebd797c96008c54d64c1ace08e7ed669f6690e3
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-02-26 21:46:46 +0200

    fixed let to const

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 'use strict'
 
 //hw_6.1
-let input1 = prompt('input1 text');
-let input2 = prompt('input2 text');
-let input3 = prompt('input3 text');
+const input1 = prompt('input1 text');
+const input2 = prompt('input2 text');
+const input3 = prompt('input3 text');
 
 console.log(input3 + ' ' + input2 + ' ' + input1);
 
 //hw_6.2
-let num = 12345;
+const num = 12345;
 
 console.log(num.toString().split('').join(' '));
