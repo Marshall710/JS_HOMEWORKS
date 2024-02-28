768ed9b153bce389a20b237ccf1bac756ca7e608
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-02-28 21:13:41 +0200

    hw_9 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-//###
+'use strict'
+
+const convert = prompt('Input temperature and temperature unit (C or F)');
+
+if (convert.toLowerCase().includes('c')) {
+    alert(parseInt(convert, 10) * 9 / 5 + 32)
+} else if (convert.toLowerCase().includes('f')) {
+    alert((parseInt(convert, 10) - 32) * 5 / 9)
+} else {
+    alert('Incorrect value')
+}
