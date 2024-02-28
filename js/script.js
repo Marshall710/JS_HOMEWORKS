d5a93f3dbe887a15614ef4ceb1d492f131fd518e
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-02-28 21:02:06 +0200

    added text in the alert

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 'use strict'
 
 const userAge = prompt('How old are you ?');
-alert(`You age ${parseInt(userAge, 10)}. After 10 years you will be ${(parseInt(userAge, 10)) + 10}!`);
+alert(`You age ${parseInt(userAge, 10)}. After 10 years you will be ${(parseInt(userAge, 10)) + 10} years old!`);
