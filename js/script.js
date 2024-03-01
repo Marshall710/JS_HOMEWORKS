45b25c967079f7cee9476ac544dcee06d65326a1
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-01 21:56:02 +0200

    hw_11 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-//###
+'use strict'
+
+const operation = prompt('Choose math operation (add, sub, mult, div)').toLowerCase();
+const operands1 = parseInt(prompt('Input first operand'), 10);
+const operands2 = parseInt(prompt('Input second operands'), 10);
+
+let result = '';
+
+if (operation === 'add') {
+    result = operands1 + operands2;
+    alert(`${operands1} + ${operands2} = ${result}`);
+} else if (operation === 'sub') {
+    result = operands1 - operands2;
+    alert(`${operands1} - ${operands2} = ${result}`)
+} else if (operation === 'multi') {
+    result = operands1 * operands2;
+    alert(`${operands1} * ${operands2} = ${result}`)
+} else if (operation === 'div') {
+    result = operands1 / operands2;
+    alert(`${operands1} / ${operands2} = ${result}`)
+} else {
+    alert('Incorrect math operations');
+}
+
