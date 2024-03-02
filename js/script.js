2bf2d7dddf09861588aeb2efe0d747b01033a543
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-02 20:30:23 +0200

    hw_12 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-//###
+'use strict'
+
+//Task1
+const user = {
+    name: 'John',
+    surname: 'Smith'
+}
+
+user.name = 'Pete'
+delete user.name;
+console.log(user);
+
+//Task2
+//так це працюватиме, тому що user зберігає посиланням на об'єкт і воно не змінилось,
+//властивості об'єкта можна змінювати
+
+//Task3
+const salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+let sum = salaries.John + salaries.Ann + salaries.Pete;
+console.log(sum);
