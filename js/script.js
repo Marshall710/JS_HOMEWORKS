df7b32735ea601cf418e87febb087cb3a600f38e
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-05 21:48:27 +0200

    hw_16 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-//###
+'use strict'
+
+const number1 = parseInt(prompt('Input first number'), 10);
+const number2 = parseInt(prompt('Input second number'), 10);
+const number3 = parseInt(prompt('Input third number'), 10);
+
+alert((number1 + number2 + number3) / 3);
