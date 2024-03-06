40afcb3f50c8a74c39e56fda52a893479088300a
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-06 20:51:33 +0200

    hw_14 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-//###
+'use strict'
+
+const operands1 = parseInt(prompt('Input first operand'), 10);
+const operands2 = parseInt(prompt('Input second operands'), 10);
+
+alert(`  
+    ${operands1} + ${operands2} = ${operands1 + operands2} 
+    ${operands1} - ${operands2} = ${operands1 - operands2} 
+    ${operands1} * ${operands2} = ${operands1 * operands2} 
+    ${operands1} / ${operands2} = ${operands1 / operands2} 
+`);
