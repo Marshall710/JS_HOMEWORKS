4765f67e6262f72831f1fa038623cc92603373b4
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-11 17:27:19 +0200

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 'use strict'
 
 const number = prompt('Input number');
-let age = '';
+let age = 'років';
 
 if (!isNaN(+number)) {
     if (number % 10 === 1 && number % 100 !== 11) {
         age = 'рік';
     } else if ((number % 10 >= 2 && number % 10 <= 4) && (number % 100 < 10 || number % 100 >= 20)) {
         age = 'роки';
-    } else {
-        age = 'років';
     }
     alert(`${number} ${age}`);
 } else {
