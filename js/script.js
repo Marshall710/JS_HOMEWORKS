50220e9aaaca32a5bd52876238bc9e886e67578e
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-11 15:44:33 +0200

    hw_19 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-//###
+'use strict'
+
+const number = prompt('Input number');
+let age = '';
+
+if (!isNaN(+number)) {
+    if (number % 10 === 1 && number % 100 !== 11) {
+        age = 'рік';
+    } else if ((number % 10 >= 2 && number % 10 <= 4) && (number % 100 < 10 || number % 100 >= 20)) {
+        age = 'роки';
+    } else {
+        age = 'років';
+    }
+    alert(`${number} ${age}`);
+} else {
+    alert('Input only number')
+}
