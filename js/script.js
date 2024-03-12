88a847565be91b0274724925f39fc1cbf0808c4e
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-12 23:15:51 +0200

    hw_26 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-//###
+'use strict'
+
+let sumD = 0;
+for(let a = 1; a <= 20; a++) {
+    if(a % 2 === 0) continue
+    document.write(`${a}<br>`)
+    sumD += a;
+}
+document.write(`Сума непарних дільників числа: ${sumD}`)
