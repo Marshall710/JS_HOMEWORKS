7e6eeec73be32f3bde62068df63ee26025ddaacb
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-13 00:12:30 +0200

    hw_23

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-//###
+'use strict'
+
+//Task 1
+// for (let a = 20; a <= 30; a += 0.5) {
+//     document.write(`${a}<br>`)
+// }
+
+//Task 2
+// for (let d = 10; d <= 100; d += 10) {
+//     document.write(`${d * 27}<br>`);
+// }
+
+//Task 3
+// let num = 2;
+// let sqr = 0;
+// for(let b = 1; b <= 100; b++) {
+//     sqr = Math.pow(b,num)
+//     // document.write(`${sqr}<br>`)
+//     if(sqr < num) {
+//         document.write(`${sqr}<br>`)
+//     }
+// }
+
+//Task 4
+
