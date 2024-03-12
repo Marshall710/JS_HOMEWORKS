81db38473851c0f9a596aa97251f1161bec1d2e5
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-12 19:46:11 +0200

    hw_24 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-//###
+'use strict'
+
+//Task 1
+const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+document.write(`Сума елементів: ${sum}</br>`);
+//Task 2
+let sum2 = 0;
+for (let i = 0; i < arr.length; i++) {
+   sum2 += (Math.pow(arr[i], 2));
+}
+document.write(`Сума квадратів: ${sum2}`)
