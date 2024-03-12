58d3fcee64968287986578eaf0e72201abb45372
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-12 20:26:52 +0200

    hw_25 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-//###
+'use strict'
+
+let intNumber = +prompt('Input number');
+// let intNumber = 60;
+if (isNaN(intNumber) || intNumber < 1) {
+    console.log('NaN');
+} else {
+    let minDivider = 2;
+        while (intNumber % minDivider !== 0) {
+            minDivider++;
+    }
+    document.write(`${minDivider}`);
+}
