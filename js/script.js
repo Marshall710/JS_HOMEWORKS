3eb71cf9fb2b9c25531d4e90ba0cf2408d25a2d4
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-13 10:44:54 +0200

    improve hw

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 'use strict'
 
-let i = 10;
+
 //Task 1
-for(i; i <= 20; i++) {
+for(let i = 10; i <= 20; i++) {
     document.write(`${i}`)
     if(i < 20) {
         document.write(', ' )
@@ -10,47 +10,47 @@ for(i; i <= 20; i++) {
 }
 
 // Task 2
-for(i; i <= 20; i++) {
+for(let i = 10; i <= 20; i++) {
     document.write(`${Math.pow(i, 2)} `)
 }
 
 //Task 3
-for (let a = 0; a < 10; a++) {
-    document.write(a * 7 + ' ');
+for (let i = 0; i < 10; i++) {
+    document.write(i * 7 + ' ');
 }
 
 //Task 4
-for(let b = 1; b <= 15; b++) {
-    document.write(`${b + b} ` + ' ')
+for(let i = 1; i <= 15; i++) {
+    document.write(`${i + i} ` + ' ')
 }
 
 //Task 5
-for(let c = 15; c <= 35; c++) {
-    document.write(c * c + ' ')
+for(let i = 15; i <= 35; i++) {
+    document.write(i * i + ' ')
 }
 
-//Task 6 ???????????????????????????
+//Task 6
 let sumMean = 0;
-for (let d = 1; d <= 500; d++) {
-    sumMean += d / 500;
+for (let i = 1; i <= 500; i++) {
+    sumMean += i ;
 }
-document.write(`${Math.floor(sumMean)}`);
+document.write(`${(sumMean) / 500}`);
 
 // Task 7
 let sum = 0;
-for (let f = 30; f <= 80; f++) {
-    if (f % 2 === 0) {
+for (let i = 30; i <= 80; i++) {
+    if (i % 2 === 0) {
          // document.write(f + ' ')
-        sum += f;
+        sum += i;
     }
 }
 document.write(`${sum}<br>`);
 
 //Task 8
-for (let g = 100; g <= 200; g++) {
-    if (g % 3 === 0) {
+for (let i = 100; i <= 200; i++) {
+    if (i % 3 === 0) {
         // document.write(g / 3 + ' ');
-         document.write(`${g}<br>` + ' ');
+         document.write(`${i}<br>` + ' ');
     }
 }
 
@@ -68,7 +68,7 @@ let evenDiv = 0;
     document.write(`Кількість парних дільників числа ${intNum}: ${counter}<br>`);
     document.write(` Сума парних дільників числа ${intNum}: ${evenDiv}<br>`);
 
-// //Task 12
+//Task 12
 for (let h = 1; h <= 10; h++) {
     for (let k = 1; k <= 10; k++) {
         document.write(`${h} * ${k} = ${h * k}<br>`);
