e729699f72eb45fd91934e2d1e7de7034b6a7cd9
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-14 22:42:22 +0200

    hw_28 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-//###
+'use strict'
+
+const number = +prompt("Введіть число!")
+const pow = +prompt("Введіть ступінь числа!")
+const powNumber = function (number, pow = 1) {
+    if (isNaN(number) || isNaN(pow)) {
+        return 'помилка';
+    } else {
+        return Math.pow(number, pow);
+    }
+}
+const result = powNumber(number, pow);
+alert(result);
