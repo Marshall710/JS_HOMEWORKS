2dac0ae4a3a29f77a13c62f4b64166a443052f6a
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-16 21:20:48 +0200

    hw_29 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-//###
+'use strict';
+
+const padString = function (string, lengthString, char = '*', left = false) {
+    if (typeof string !== 'string' || !Number(lengthString) || typeof char !== 'string' || lengthString < 0) {
+        return `помилка: ${string}: має бути рядком, ${lengthString}: має бути числом, ${char} має бути символом`;
+    } else if (string.length >= lengthString) {
+        return string.substr(0, lengthString);
+    } else {
+        return left ? string.padStart(lengthString, char) : string.padEnd(lengthString, char);
+    }
+}
+
+console.log(padString('hello', 8));
+console.log(padString('hello', 6, '*', true));
+console.log(padString('hello', 2));
+
+
