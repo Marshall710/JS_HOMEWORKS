8c5250b8028f11ffabbbd971634cf1f70b1cae9a
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-16 19:57:12 +0200

    hw_27 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-//###
+'use strict'
+
+let result = '';
+const generateKey = function (length, characters) {
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters[randomIndex];
+    }
+    return result;
+}
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+console.log(key);
