dd0c70b5902e4f4f50c3fe45367e37b2327f388f
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-16 23:06:31 +0200

    hw_30 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-//###
+'use strict'
+
+const arr = [1, 2, 3, -1, -2, -3];
+const positiveArr = function (arr) {
+    const newArr = []
+
+    if(arr.length === 0) {
+        return 'array is empty!'
+    }
+
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            newArr.push(arr[i])
+        } else {
+            console.log('всі інші негативні:', arr[i]);
+        }
+    }
+
+    if (newArr.length > 0) {
+        return newArr
+    } else {
+        return null;
+    }
+}
+
+console.log(positiveArr(arr));
