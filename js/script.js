99177a6819acc5238b0e56a4381f370dd5a80d5d
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-17 13:59:54 +0200

    hw_30 refactoring

--- js/script.js
+++ js/script.js
@@ -3,10 +3,6 @@
 const number = +prompt("Введіть число!")
 const pow = +prompt("Введіть ступінь числа!")
 const powNumber = function (number, pow = 1) {
-    if (isNaN(number) || isNaN(pow)) {
-        return 'помилка';
-    } else {
-        return Math.pow(number, pow);
-    }
+   return (isNaN(null) || isNaN(pow)) ? 'помилка' : Math.pow(number, pow);
 }
 alert(powNumber(number, pow));
