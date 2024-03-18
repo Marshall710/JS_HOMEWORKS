c32986810cd9d3a8ff7752d5bba131ca2a11a20c
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-18 08:28:23 +0200

    hw_30 added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const arr = [1, 2, 3, -1, -2, -3];
 const positiveArr = function (arr) {
     const newArr = []
     if(arr.length === 0) {
-        return 'array is empty!'
+        return 'масив порожній'
+    } else if (!Array.isArray(arr)) {
+        return 'аргумент має бути масивом!'
     }
-
     for(let i = 0; i < arr.length; i++) {
         if (arr[i] > 0) {
             newArr.push(arr[i])
@@ -14,7 +14,8 @@ const positiveArr = function (arr) {
             console.log('всі інші негативні:', arr[i]);
         }
     }
-
     return (newArr.length > 0) ? newArr : null;
 }
+
+const arr = [1, 2, 3, -1, -2, -3];
 console.log(positiveArr(arr));
