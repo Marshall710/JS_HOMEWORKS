9e4b2fc0da43c183aff9efa527ddf89f3c9005ed
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-03-25 11:33:28 +0200

    hw_33 done

---
+++ index.js
@@ -0,0 +1,15 @@
+const sum = function (a, b) {
+  return a + b;
+};
+
+const sub = function (a, b) {
+  return a - b;
+};
+
+const mult = function (a, b) {
+  return a * b;
+};
+
+console.log(sum(1, 3));
+console.log(sub(1, 3));
+console.log(mult(1, 3));
