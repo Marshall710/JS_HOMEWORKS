cb7697b9bf8fa0b972247eb4d31b629943417c55
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-04-02 21:56:00 +0300

    hw_39 done

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,8 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  rules: {},
+  rules: {
+    "linebreak-style": "off",
+    "indent": "off"
+  },
 };
--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-'use strict'
+'use strict';
 
+const arr = [4, 5, 2, 1, 3];
 // indexOf
 const indexOf = function (arr, searchIndex) {
     for (let i = 0; i < arr.length; i++) {
@@ -8,9 +9,9 @@ const indexOf = function (arr, searchIndex) {
         }
     }
     return -1;
-}
-const arr = [4, 5, 2, 1, 3]
-// console.log(indexOf(arr, 8));
+};
+
+console.log(indexOf(arr, 1));
 
 //lastIndexOf
 const lastIndexOf = function (arr, searchIndex) {
@@ -20,21 +21,23 @@ const lastIndexOf = function (arr, searchIndex) {
         }
     }
     return -1;
-}
-// console.log(lastIndexOf(arr, 4));
+};
+
+console.log(lastIndexOf(arr, 5));
 
 //find
-// const find = function (arr, callback) {
-//     for (let i = 0; i < arr.length; i++) {
-//         if (callback(arr[i], i, arr)) {
-//             return arr[i];
-//         }
-//     }
-//     return undefined;
-// }
-// find(arr, function(element) {
-//     console.log(element > 1)
-// });
+const find = function (arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i], i, arr)) {
+            return arr[i];
+        }
+    }
+    return undefined;
+};
+
+console.log(find(arr, function(item, index, arr) {
+    return item > 5;
+}));
 
 //includes
 const includes = function (arr, index) {
@@ -44,7 +47,8 @@ const includes = function (arr, index) {
         }
     }
     return false;
-}
+};
+
 console.log(includes(arr, 1));
 
 //every
@@ -55,9 +59,23 @@ const every = function (arr, callback) {
         }
     }
     return true;
-}
+};
 
-console.log(every(arr, function (element, index, array) {
+console.log(every(arr, function (item, index, arr) {
+    return item < 10;
+}));
 
+//some
+const some = function (arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        if(callback(arr[i], i, arr)) {
+            return true;
+        }
+    }
+    return false;
+};
+
+console.log(some(arr, function (item, index, arr){
+    return item > 1;
 }));
 
