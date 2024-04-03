c864e25571ae4ee97cced9d75b427b58920545c8
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-04-03 10:33:41 +0300

    hw_39 done

--- js/script.js
+++ js/script.js
@@ -1,81 +1,99 @@
-'use strict';
+"use strict";
 
 const arr = [4, 5, 2, 1, 3];
 // indexOf
 const indexOf = function (arr, searchIndex) {
-    for (let i = 0; i < arr.length; i++) {
-        if(arr[i] === searchIndex) {
-            return i;
-        }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === searchIndex) {
+      return i;
     }
-    return -1;
+  }
+  return -1;
 };
 
 console.log(indexOf(arr, 1));
 
 //lastIndexOf
 const lastIndexOf = function (arr, searchIndex) {
-    for (let i = arr.length - 1; i >= 0; i--) {
-        if (arr[i] === searchIndex) {
-            return i;
-        }
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] === searchIndex) {
+      return i;
     }
-    return -1;
+  }
+  return -1;
 };
 
 console.log(lastIndexOf(arr, 5));
 
 //find
 const find = function (arr, callback) {
-    for (let i = 0; i < arr.length; i++) {
-        if (callback(arr[i], i, arr)) {
-            return arr[i];
-        }
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      return arr[i];
     }
-    return undefined;
+  }
+  return undefined;
 };
 
-console.log(find(arr, function(item, index, arr) {
+console.log(
+  find(arr, function (item) {
     return item > 5;
-}));
+  }),
+);
 
 //includes
 const includes = function (arr, index) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === index) {
-            return true;
-        }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === index) {
+      return true;
     }
-    return false;
+  }
+  return false;
 };
 
 console.log(includes(arr, 1));
 
 //every
 const every = function (arr, callback) {
-    for (let i = 0; i < arr.length; i++) {
-        if (!callback(arr[i], i, arr)) {
-            return false;
-        }
+  for (let i = 0; i < arr.length; i++) {
+    if (!callback(arr[i], i, arr)) {
+      return false;
     }
-    return true;
+  }
+  return true;
 };
 
-console.log(every(arr, function (item, index, arr) {
+console.log(
+  every(arr, function (item) {
     return item < 10;
-}));
+  }),
+);
 
 //some
 const some = function (arr, callback) {
-    for (let i = 0; i < arr.length; i++) {
-        if(callback(arr[i], i, arr)) {
-            return true;
-        }
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      return true;
     }
-    return false;
+  }
+  return false;
 };
 
-console.log(some(arr, function (item, index, arr){
+console.log(
+  some(arr, function (item) {
     return item > 1;
-}));
+  }),
+);
 
+//reduce
+const reduce = function (arr, callback, accumulator) {
+  let result = accumulator;
+  for (let i = 0; i < arr.length; i++) {
+    result = callback(result, arr[i], i, arr);
+  }
+  return result;
+};
+
+console.log(reduce(arr, function (accum, index) {
+    return accum + index;
+},0));
