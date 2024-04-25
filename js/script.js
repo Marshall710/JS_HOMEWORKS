550009a911d44b74e3879f9936c0020b79d3a517
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-04-25 21:00:19 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 (function () {
-  const sum = () => {
-    let number = 0;
+  const sum = (param) => {
+    let number = param;
 
     const result = (value) => (number += value);
 
     return result;
   };
 
-  const sumFunc = sum();
+  const sumFunc = sum(0);
 
   console.log(sumFunc(3));
   console.log(sumFunc(5));
   console.log(sumFunc(20));
-})();
+}());
