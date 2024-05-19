1f9024100bab03613f5a5f335027c3a1a6617bd8
Oleksandr Kulinichenko <cawa12344@gmail.com>
2024-05-19 20:08:22 +0300

    hw_63 done

--- js/script.js
+++ js/script.js
@@ -2,30 +2,30 @@
   const categories = [
     { id: 1, name: 'Hyundai' },
     { id: 2, name: 'Honda' },
-    { id: 3, name: 'Shkoda' },
+    { id: 3, name: 'Škoda' },
   ];
 
   const products = {
     1: [
       { id: 1, name: 'Двигун 1,4 DSI АКПП', description: 'В наявності' },
-      { id: 2, name: 'Двигун 1,6 DSI АКПП', description: 'Очікується поставка' },
+      { id: 2, name: 'Двигун 1,6 DSI АКПП', description: 'В наявності' },
     ],
     2: [
       { id: 3, name: 'Двигун 1,8 Гібрид АКПП', description: 'В наявності' },
-      { id: 4, name: 'Двигун 1,2 Бензин АКПП', description: 'Очікується поставка' },
+      { id: 4, name: 'Двигун 1,2 Бензин АКПП', description: 'В наявності' },
     ],
     3: [
       { id: 5, name: 'Двигун 1,6 DSI АКПП', description: 'В наявності' },
-      { id: 6, name: 'Двигун 1,4 DSI МКПП', description: 'Очікується поставка' },
+      { id: 6, name: 'Двигун 1,4 DSI МКПП', description: 'В наявності' },
     ],
   };
 
-  const productList = document.getElementById('product-list');
-  const productInfo = document.getElementById('product-details');
+  const productList = document.querySelector('[data-product-list]');
+  const productInfo = document.querySelector('[data-product-details]');
   const buyButton = document.getElementById('buy-button');
 
   const loadCategories = function () {
-    const categoryList = document.getElementById('category-list');
+    const categoryList = document.querySelector('[data-category-list]');
     categoryList.innerHTML = '';
     categories.forEach((category) => {
       const li = document.createElement('li');
@@ -56,13 +56,6 @@
 
   const buyProduct = function () {
     alert('Товар куплений');
-    reset();
-  };
-
-  const reset = function () {
-    productList.innerHTML = '';
-    productInfo.innerHTML = '';
-    buyButton.style.display = 'none';
   };
 
   loadCategories();
